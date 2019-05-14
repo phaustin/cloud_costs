b00433529888784f54257c2d2a53e6d87989eb24
Phil Austin <paustin@eos.ubc.ca>
2019-05-13 18:29:30 -0700

    run black

--- python/rough_costs.py
+++ python/rough_costs.py
@@ -31,14 +31,11 @@
 #     toc_section_display: true
 #     toc_window_display: true
 # ---
-
 # %% [markdown] {"toc": true}
 # <h1>Table of Contents<span class="tocSkip"></span></h1>
 # <div class="toc"><ul class="toc-item"><li><span><a href="#Summary" data-toc-modified-id="Summary-1">Summary</a></span></li><li><span><a href="#Example" data-toc-modified-id="Example-2">Example</a></span><ul class="toc-item"><li><span><a href="#Result" data-toc-modified-id="Result-2.1">Result</a></span></li></ul></li><li><span><a href="#Machines-needed-for-users" data-toc-modified-id="Machines-needed-for-users-3">Machines needed for users</a></span></li><li><span><a href="#Read-in-the-price-list" data-toc-modified-id="Read-in-the-price-list-4">Read in the price list</a></span></li><li><span><a href="#Input-case-parameters" data-toc-modified-id="Input-case-parameters-5">Input case parameters</a></span></li><li><span><a href="#Calculate-daily-and-total-costs" data-toc-modified-id="Calculate-daily-and-total-costs-6">Calculate daily and total costs</a></span></li><li><span><a href="#Output" data-toc-modified-id="Output-7">Output</a></span></li></ul></div>
-
 # %% [markdown]
 # # Cost calculation for Google Compute
-
 # %% [markdown]
 # ## Summary
 #
@@ -59,9 +56,8 @@
 #
 # This notebook reads those csv files then, given a machine type like "n1-standard-64"
 # calculates the cost per student for a particular number of days/ram per student/storage per student
-
 # %% [markdown]
-# ## Example 
+# ## Example
 #
 # 1000 students during a 4 month term using the cloud notebooks
 # for 4 hours per day.
@@ -85,34 +81,33 @@
 # Disk space cost in $/student=$ 0.48
 # Machine_cost in $/student=$13.13
 # ```
-
 # %% [markdown]
 # ## Machines needed for users
-
 # %%
-import pandas as pd
 import numpy as np
+import pandas as pd
+
 
 def calculate_machines_needed(users, mem_per_user, active_machine):
     """
     Parameters
     ----------
-    
+
     users: int
        number of users
     mem_per_user: float
        ram needed per users
     active_machine: pd.Series
        machine characteristics
-       
+
     Returns
     -------
-    
+
     total_machines_needed: int
        number of machines needed for hosting
     """
-    
-    memory_per_machine = active_machine['Memory']
+
+    memory_per_machine = active_machine["Memory"]
     total_gigs_needed = users * mem_per_user
     total_machines_needed = int(np.ceil(total_gigs_needed / memory_per_machine))
     return total_machines_needed
@@ -124,8 +119,8 @@ def calculate_machines_needed(users, mem_per_user, active_machine):
 # These csv files were written out by the `write_costs_csvs.py` script
 
 # %%
-n1_machines=pd.read_csv('n1_machines.csv',index_col='Machine type')
-disks = pd.read_csv('disks.csv',index_col="Type")
+n1_machines = pd.read_csv("n1_machines.csv", index_col="Machine type")
+disks = pd.read_csv("disks.csv", index_col="Type")
 
 # %%
 disks
@@ -137,30 +132,34 @@ n1_machines
 # ## Input case parameters
 
 # %%
-num_users=1000
-ram_per_user=2.  #Gbytes
-space_per_user=3 #Gbytes
-machine_type='n1-standard-64'
-number_of_days=4*30
+num_users = 1000
+ram_per_user = 2.0  # Gbytes
+space_per_user = 3  # Gbytes
+machine_type = "n1-standard-64"
+number_of_days = 4 * 30
 hours_per_day = 4
-day_fraction = hours_per_day/24.
+day_fraction = hours_per_day / 24.0
 
 # %% [markdown]
 # ## Calculate daily and total costs
 
 # %%
 active_machine = n1_machines.loc[machine_type]
-machine_cost_daily=active_machine['Price (USD / hr)']*24. #cost per day per machine
-num_machines=calculate_machines_needed(num_users,ram_per_user,active_machine)
-disk_cost_daily=disks.loc['Standard provisioned space']['Price (per GB / month)']/30.  #Gbyte/day
-total_storage_cost=disk_cost_daily*number_of_days*num_users*space_per_user
-total_cpu_cost=machine_cost_daily*number_of_days*num_machines*day_fraction
+machine_cost_daily = (
+    active_machine["Price (USD / hr)"] * 24.0
+)  # cost per day per machine
+num_machines = calculate_machines_needed(num_users, ram_per_user, active_machine)
+disk_cost_daily = (
+    disks.loc["Standard provisioned space"]["Price (per GB / month)"] / 30.0
+)  # Gbyte/day
+total_storage_cost = disk_cost_daily * number_of_days * num_users * space_per_user
+total_cpu_cost = machine_cost_daily * number_of_days * num_machines * day_fraction
 
 # %% [markdown]
 # ## Output
 
 # %%
-message=f"""
+message = f"""
 number of users: {num_users}
 ram per user {ram_per_user} Gbytes
 
@@ -178,11 +177,10 @@ cpu cost: ${total_cpu_cost}
 print(message)
 
 # %%
-text=f"""
+text = f"""
 Disk space cost: $/student=${total_storage_cost/num_users:5.2f}
 
 Machine_cost: $/student=${total_cpu_cost/num_users:5.2f}
 """
 
 print(text)
-
--- write_cost_csvs.py
+++ write_cost_csvs.py
@@ -3,18 +3,17 @@ adapted from  https://github.com/jupyterhub/zero-to-jupyterhub-k8s/blob/master/d
 Screen scapes https://cloud.google.com/compute/pricing and returns data frames with
 standard machine costs (n1_machines) and storage costs (disks)
 """
-import pandas as pd
 import numpy as np
-from google_costs import disk_df, machine_df
-disk_df.to_csv('disks.csv',index=False)
-machine_df['Machine type'] = [item.strip() for item in machine_df['Machine type']]
-machine_df['Memory']=[item.replace('GB','') for item in machine_df['Memory']]
-machine_df['Memory']=[float(item) for item in machine_df['Memory']]
-machine_df.set_index('Machine type',drop=False,inplace=True)
-n1_list=[item for item in machine_df.index if item.find('n1-standard') > -1]
+import pandas as pd
+
+from google_costs import disk_df
+from google_costs import machine_df
+
+disk_df.to_csv("disks.csv", index=False)
+machine_df["Machine type"] = [item.strip() for item in machine_df["Machine type"]]
+machine_df["Memory"] = [item.replace("GB", "") for item in machine_df["Memory"]]
+machine_df["Memory"] = [float(item) for item in machine_df["Memory"]]
+machine_df.set_index("Machine type", drop=False, inplace=True)
+n1_list = [item for item in machine_df.index if item.find("n1-standard") > -1]
 n1_machines = machine_df.loc[n1_list]
-n1_machines.to_csv('n1_machines.csv')
-
-
-
-
+n1_machines.to_csv("n1_machines.csv")
